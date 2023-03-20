c19fb22e205ff22da8e0629479b4cda303675911
Anurag Pawar <anuragppawar72@gmail.com>
2023-03-20 12:58:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const getRandomUpper = () =>
   String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 
 const getRandomNumber = () =>
-  String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  String.fromCharCode(Math.floor(Math.random() * 9) + 48);
 
 const getRandomSymbol = () => {
   const symbols = "!@#$%^&*(){}[]=<>/,.";
